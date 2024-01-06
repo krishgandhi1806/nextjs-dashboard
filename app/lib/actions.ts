de444591b25a187d358fa4d7fe3bba301d4cb2e0
Krish Gandhi <gandhikrish1806@gmail.com>
2024-01-06 18:56:54 +0530

    Added Authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,28 +1,55 @@
 'use server';
 import { sql } from "@vercel/postgres";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 
 
+
 const FormSchema= z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+      invalid_type_error: 'Please select a customer'
+    }),
+    amount: z.coerce.number().gt(0, {message: 'Please enter an amount greater than $0'}),
+    status: z.enum(['pending', 'paid'],{
+      invalid_type_error: 'Please select an invoice status'
+    }),
     date: z.string()
 
 })
 
+
 const CreateInvoice= FormSchema.omit({id: true, date: true});
 
-export async function createInvoice(formData: FormData){
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-    const {customerId, amount, status}= CreateInvoice.parse({
+export async function createInvoice(prevState:State, formData: FormData){
+
+    const validatedFields= CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
     });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
 
     const amountInCents= amount *100;
     const date= new Date().toISOString().split('T')[0];
@@ -53,12 +80,21 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 // ...
  
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+  
+  const { customerId, amount, status } = validatedFields.data;
+  
   const amountInCents = amount * 100;
  
   try{
@@ -88,3 +124,23 @@ export async function deleteInvoice(id: string) {
     }
   }
   }
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+){
+  try{
+    await signIn('credentials', formData);
+  } catch(error){
+    if(error instanceof AuthError){
+      switch(error.type){
+        case 'CredentialsSignin':
+          return 'Invalid Credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
